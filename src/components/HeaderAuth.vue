133976e69ea4b55af53b23ba564f7c2cc4c10a77
Asahi-cloud-lang <hiroto_asahi@yahoo.co.jp>
2021-05-16 19:09:28 +0900

    first-commit

---
+++ src/components/HeaderAuth.vue
@@ -0,0 +1,45 @@
+<template>
+ <div class="header flex">
+   <div class="right center flex">
+     <img src="../assets/rese.png">
+     <p>RESE</p>
+   </div>
+   <div class="left center flex">
+     <p @click="$router.push('/signup')">Registration</p>
+     <p @click="$router.push('/')">Login</p>
+   </div>
+ </div>
+</template>
+
+<style scoped>
+.center {
+ align-items: center;
+}
+
+.right p {
+ font-size:30px;
+ font-weight: bolder;
+ color: blue;
+}
+
+.right img {
+  border-radius: 5px;
+  margin: 0px 20px 0px 50px;
+  height: 30px;
+}
+
+.left p {
+ margin-right: 20px;
+ font-weight: bold;
+ cursor: pointer;
+}
+
+.header {
+ margin: 20px;
+}
+
+.flex {
+ display: flex;
+ justify-content: space-between;
+}
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <HeaderAuth />
+    <div class="card">
+      <p>Registration</p>
+      <div class="form">
+        <div>
+          <img src="../assets/person.png">
+          <input placeholder="Username" type="text" />
+        </div>
+        <div>
+          <img src="../assets/mail.png">
+          <input placeholder="Email" type="email" />
+        </div>
+        <div>
+          <img src="../assets/rock.png">
+          <input placeholder="password" type="password" />
+        </div>
+        <div>
+          <button @click="auth">登録</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderAuth from "../components/HeaderAuth";
+export default {
+  components: {
+    HeaderAuth
+  }
+};
+</script>
+
+<style scoped>
+button {
+  width: 100px;
+  text-align: center;
+  padding: 8px 0 10px;
+  color: #fff;
+  background-color: #5419da;
+  border-radius: 25px;
+  cursor: pointer;
+}
+.card {
+  margin: 100px auto;
+  width: 350px;
+  border-radius: 10px;
+  box-shadow: 2px 2px 4px gray;
+}
+.card p {
+  font-weight: bold;
+  color: #fff;
+  background-color: blue;
+  padding: 20px;
+  border-radius: 10px 10px 0px 0px;
+}
+input {
+  width: 70%;
+  padding: 10px 10px 3px 0px;
+  border: none;
+  border-bottom: 1px solid black;
+  color: black;
+  line-height: 1rem;
+}
+.form {
+  text-align: center;
+}
+
+.form img {
+  vertical-align: bottom;
+  padding-right: 10px;
+  margin-top: 10px;
+}
+
+.form div:nth-child(1) {
+  padding: 20px 10px 0px;
+}
+
+.form div:nth-child(2) {
+  padding: 10px 10px 10px;
+}
+
+.form div:nth-child(3) {
+  padding: 0px 10px 20px;
+}
+
+.form div:nth-child(4) {
+  text-align: right;
+}
+
+.form button {
+  margin: 15px;
+  background-color: blue;
+  border-radius: 10px;
+  border: none;
+}
+</style>
